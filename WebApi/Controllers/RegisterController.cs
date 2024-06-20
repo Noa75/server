4157441ed84aec6dd71e1a5b5a49d8c3c5986ad6
Noa75 <noacohen75@gmail.com>
2024-06-20 18:56:08 +0300

    20.6

---
+++ WebApi/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class registerController : ControllerBase
+    {
+
+        RealocationAppContext db = new RealocationAppContext();
+        [HttpPost]
+        public async Task<ActionResult<User>> Register([FromBody] User user)
+        {
+            if (await db.Users.AnyAsync(u => u.Username == user.Username || user.Username == "" || user.PasswordHash == ""))
+            {
+                Console.WriteLine(user.Email);
+                return BadRequest("Username already exists.");
+            }
+
+            user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(user.PasswordHash);
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+
+            // Return a success message or the created user object
+            return Ok(new { message = "Registration successful!", user });
+        }
+    }
+}
